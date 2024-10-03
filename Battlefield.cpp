d839c6d9475c016551d702189c104c7e1b44a54b
Kah Yong <cky734@gmail.com>
2024-10-03 21:08:11 +0800

    Remove names and details

--- Battlefield.cpp
+++ Battlefield.cpp
@@ -1,15 +1,3 @@
-/**********|**********|**********|
-Program: Battlefield.cpp
-Course: Data Structures and Algorithms
-Trimester: 2410
-Name: CHIN ZHEN HO , ERIC TEOH WEI XIANG , BERNARD RYAN SIM KANG XUAN , CHAN KAH YONG
-ID: 1221102540 , 1221102007 , 1221101777 , 1231302796
-Lecture Section: TC2L
-Tutorial Section: TT6L
-Email: 1221102540@soffice.mmu.edu.my , 1221102007@soffice.mmu.edu.my , 1221101777@soffice.mmu.edu.my , 1231302796@soffice.mmu.edu.my
-Phone: 0182067278 , 0174063708 , 0167168548 , 0109586656
-**********|**********|**********/
-
 #include "Battlefield.h"
 #include <iomanip>
 
